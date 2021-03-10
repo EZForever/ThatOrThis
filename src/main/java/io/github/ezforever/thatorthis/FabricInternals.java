e6c23619a3f51a1931dcd2a3ee3ec5cf9e14a7fb
EZForever <34133756+EZForever@users.noreply.github.com>
2021-03-10 15:07:49 +0800

    Add experimental support on loading LanguageAdapter mods
    Allows language support mods and GrossFabricHacks prePrePreLaunch to work even when loaded via ThatOrThis. Not recommended to use since almost no testing is done.
    
    Also cleared warnings in FabricInternals.java

--- src/main/java/io/github/ezforever/thatorthis/FabricInternals.java
+++ src/main/java/io/github/ezforever/thatorthis/FabricInternals.java
@@ -2,6 +2,7 @@ package io.github.ezforever.thatorthis;
 
 import net.fabricmc.loader.FabricLoader;
 import net.fabricmc.loader.ModContainer;
+import net.fabricmc.loader.api.LanguageAdapter;
 import net.fabricmc.loader.discovery.*;
 import net.fabricmc.loader.gui.FabricGuiEntry;
 import net.fabricmc.loader.launch.common.FabricLauncherBase;
@@ -61,15 +62,16 @@ public class FabricInternals {
                         // XXX: This is a copy of ModResolver routines, not a good idea
                         // NOTE: Assumed .jar mods as implied in findCandidates()
                         Path path = UrlUtil.asPath(url).normalize();
-                        Path modJson = FileSystemUtil.getJarFileSystem(path, false).get()
-                                .getPath("fabric.mod.json");
+                        Path modJson = FileSystemUtil.getJarFileSystem(path, false).get().getPath("fabric.mod.json");
                         LoaderModMetadata info = ModMetadataParser.parseMetadata(LOGGER, modJson);
                         if(!callback.apply(info))
                             return;
                     } catch (Throwable ignored) {
                         // Ignored; any exception that might happen here will be processed later in ModResolver.resolve(), so nothing to do
                     }
-                    ((BiConsumer<URL, Boolean>) args[1]).accept(url, requiresRemap);
+
+                    BiConsumer<URL, Boolean> urlProposer = cast(args[1]);
+                    urlProposer.accept(url, requiresRemap);
                 });
             } else {
                 return method.invoke(finder, args);
@@ -84,6 +86,7 @@ public class FabricInternals {
     private static final FabricLoader loader;
     private static final Method addModMethod;
     private static final Field modsField;
+    private static final Field adapterMapField;
 
     static {
         try {
@@ -95,9 +98,11 @@ public class FabricInternals {
 
             modsField = loader.getClass().getDeclaredField("mods");
             modsField.setAccessible(true);
+
+            adapterMapField = loader.getClass().getDeclaredField("adapterMap");
+            adapterMapField.setAccessible(true);
         } catch (NoSuchMethodException | NoSuchFieldException e) {
-            LOGGER.error("Failed to get reference to fabric-loader internals.", e);
-            throw new IllegalStateException(e);
+            throw new RuntimeException("Failed to get reference to fabric-loader internals.", e);
         }
     }
 
@@ -123,18 +128,23 @@ public class FabricInternals {
         }
     }
 
+    // This has been used a lot
+    @SuppressWarnings("unchecked")
+    static <T> T cast(Object obj) {
+        return (T)obj;
+    }
+
     static void hook(Map<String, Set<String>> modDirs) {
         try {
-            List<ModContainer> original = (List<ModContainer>) modsField.get(loader);
-            List<ModContainer> proxied = (List<ModContainer>) Proxy.newProxyInstance(
+            List<ModContainer> original = cast(modsField.get(loader));
+            List<ModContainer> proxied = cast(Proxy.newProxyInstance(
                     loader.getClass().getClassLoader(),
                     original.getClass().getInterfaces(),
                     new HookedModContainerList(original, modDirs)
-            );
+            ));
             modsField.set(loader, proxied);
         } catch (IllegalAccessException e) {
-            LOGGER.error("Mods list hook failed", e);
-            throw new IllegalStateException(e);
+            throw new RuntimeException("Mods list hook failed", e);
         }
     }
 
@@ -142,8 +152,7 @@ public class FabricInternals {
         try {
             modsField.set(loader, hook.list);
         } catch (IllegalAccessException e) {
-            LOGGER.error("Mods list unhook failed", e);
-            throw new IllegalStateException(e);
+            throw new RuntimeException("Mods list unhook failed", e);
         }
     }
 
@@ -157,80 +166,112 @@ public class FabricInternals {
     }
 
     private static void injectMods(Map<String, Set<String>> modDirs, Set<String> loadedModIds) {
-        try {
-            ModResolver resolver = new ModResolver();
+        ModResolver resolver = new ModResolver();
 
-            // Keep original mods' directories only for resolving dependencies
-            resolver.addCandidateFinder(new ClasspathModCandidateFinder());
-            resolver.addCandidateFinder(new DirectoryModCandidateFinder(
-                    loader.getModsDir(),
-                    loader.isDevelopmentEnvironment()
-            ));
+        // Keep original mods' directories only for resolving dependencies
+        resolver.addCandidateFinder(new ClasspathModCandidateFinder());
+        resolver.addCandidateFinder(new DirectoryModCandidateFinder(
+                loader.getModsDir(),
+                loader.isDevelopmentEnvironment()
+        ));
 
-            modDirs.forEach((String modDir, Set<String> blacklist) -> {
-                Path dir = loader.getModsDir().resolve(modDir);
-                // DirectoryModCandidateFinder creates missing directories, which is not intended
-                if(Files.exists(dir) && Files.isDirectory(dir)) {
-                    ModCandidateFinder finder = new DirectoryModCandidateFinder(dir, loader.isDevelopmentEnvironment());
-                    if(blacklist != null) {
-                        finder = (ModCandidateFinder) Proxy.newProxyInstance(
-                                loader.getClass().getClassLoader(),
-                                finder.getClass().getInterfaces(),
-                                new HookedDirectoryModCandidateFinder(
-                                        (DirectoryModCandidateFinder) finder,
-                                        (LoaderModMetadata info) -> {
-                                            if(blacklist.contains(info.getId())) {
-                                                LOGGER.debug("Skipping mod {} as per user request", info.getId());
-                                                return false;
-                                            } else {
-                                                return true;
-                                            }
+        modDirs.forEach((String modDir, Set<String> blacklist) -> {
+            Path dir = loader.getModsDir().resolve(modDir);
+            // DirectoryModCandidateFinder creates missing directories, which is not intended
+            if(Files.exists(dir) && Files.isDirectory(dir)) {
+                ModCandidateFinder finder = new DirectoryModCandidateFinder(dir, loader.isDevelopmentEnvironment());
+                if(blacklist != null) {
+                    finder = (ModCandidateFinder) Proxy.newProxyInstance(
+                            loader.getClass().getClassLoader(),
+                            finder.getClass().getInterfaces(),
+                            new HookedDirectoryModCandidateFinder(
+                                    (DirectoryModCandidateFinder) finder,
+                                    (LoaderModMetadata info) -> {
+                                        if(blacklist.contains(info.getId())) {
+                                            LOGGER.debug("Skipping mod {} as per user request", info.getId());
+                                            return false;
+                                        } else {
+                                            return true;
                                         }
-                                )
-                        );
-                    }
-                    resolver.addCandidateFinder(finder);
-                } else {
-                    LOGGER.warn("Skipping missing/invalid directory: " + modDir);
+                                    }
+                            )
+                    );
                 }
-            });
+                resolver.addCandidateFinder(finder);
+            } else {
+                LOGGER.warn("Skipping missing/invalid directory: " + modDir);
+            }
+        });
 
-            Map<String, ModCandidate> candidateMap = resolver.resolve(loader);
-            Collection<ModCandidate> candidates = candidateMap.keySet().stream()
-                    .filter((String modid) -> !loadedModIds.contains(modid))
-                    .map(candidateMap::get)
+        Collection<ModCandidate> candidates;
+        try {
+            candidates = resolver.resolve(loader)
+                    .entrySet().stream()
+                    .filter((Map.Entry<String, ModCandidate> candidate) -> !loadedModIds.contains(candidate.getKey()))
+                    .map(Map.Entry::getValue)
                     .collect(Collectors.toList());
-
-            String modText;
-            switch (candidates.size()) {
-                case 0:
-                    modText = "Loading {} additional mods";
-                    break;
-                case 1:
-                    modText = "Loading {} additional mod: {}";
-                    break;
-                default:
-                    modText = "Loading {} additional mods: {}";
-                    break;
-            }
-
-            LOGGER.info("[ThatOrThis] " + modText, candidates.size(), candidates.stream()
-                    .map((ModCandidate candidate) -> String.format("%s@%s", candidate.getInfo().getId(), candidate.getInfo().getVersion().getFriendlyString()))
-                    .collect(Collectors.joining(", ")));
-
-            if (loader.isDevelopmentEnvironment())
+            if(loader.isDevelopmentEnvironment())
                 candidates = RuntimeModRemapper.remap(candidates, ModResolver.getInMemoryFs());
-            for(ModCandidate candidate : candidates) {
-                addModMethod.invoke(loader, candidate);
-                FabricLauncherBase.getLauncher().propose(candidate.getOriginUrl());
-            }
-        } catch (ModResolutionException e) {
+        } catch(ModResolutionException e) {
             FabricGuiEntry.displayCriticalError(e, true);
-            //LOGGER.error("Unable to resolve additional mods", e);
-            //throw new IllegalStateException(e);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            LOGGER.error("Failed to inject mod into fabric-loader", e);
+            return; // Never reached
+        }
+
+        String modText;
+        switch (candidates.size()) {
+            case 0:
+                modText = "Loading {} additional mods";
+                break;
+            case 1:
+                modText = "Loading {} additional mod: {}";
+                break;
+            default:
+                modText = "Loading {} additional mods: {}";
+                break;
+        }
+
+        LOGGER.info("[ThatOrThis] " + modText, candidates.size(), candidates.stream()
+                .map((ModCandidate candidate) -> String.format("%s@%s", candidate.getInfo().getId(), candidate.getInfo().getVersion().getFriendlyString()))
+                .collect(Collectors.joining(", ")));
+
+        List<ModCandidate> candidatesToFix = new ArrayList<>();
+        for(ModCandidate candidate : candidates) {
+            // Check for LanguageAdapter incompatibility
+            if(!candidate.getInfo().getLanguageAdapterDefinitions().isEmpty()) {
+                LOGGER.warn("LanguageAdapter found in mod {}! Will try to fix later", candidate.getInfo().getId());
+                candidatesToFix.add(candidate);
+            }
+            //if(!candidate.getInfo().getEntrypoints("gfh:prePrePreLaunch").isEmpty())
+            //    LOGGER.error("GrossFabricHacks prePrePreLaunch entrypoint found in mod {}! Expect it to break", candidate.getInfo().getId());
+
+            try {
+                addModMethod.invoke(loader, candidate);
+            } catch(IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException("Failed to inject mod into fabric-loader", e);
+            }
+            FabricLauncherBase.getLauncher().propose(candidate.getOriginUrl());
+        }
+
+        Map<String, LanguageAdapter> adapterMap;
+        try {
+            adapterMap = cast(adapterMapField.get(loader));
+        } catch (IllegalAccessException e) {
+            LOGGER.error("Failed to fix LanguageAdapter mods", e);
             throw new IllegalStateException(e);
+        }
+
+        // NOTE: Fix here after injecting all mods, so mods that "ping" other mods (e.g. GrossFabricHacks) can have a good time
+        for(ModCandidate candidate : candidatesToFix) {
+            for(Map.Entry<String, String> entry : candidate.getInfo().getLanguageAdapterDefinitions().entrySet()) {
+                if(adapterMap.containsKey(entry.getKey()))
+                    throw new RuntimeException("Duplicate language adapter key: " + entry.getKey() + "! (" + entry.getValue() + ", " + adapterMap.get(entry.getKey()).getClass().getName() + ")");
+
+                try {
+                    adapterMap.put(entry.getKey(), (LanguageAdapter)Class.forName(entry.getValue(), true, FabricLauncherBase.getLauncher().getTargetClassLoader()).getDeclaredConstructor().newInstance());
+                } catch(Exception e) {
+                    throw new RuntimeException("Failed to instantiate language adapter: " + entry.getKey(), e);
+                }
+            }
         }
     }
 }
